b056ff991f744f4d3e7ac880ca56c22334c89609
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2022-11-05 21:01:09 +0100

    fix: add policy

--- infra/lib/user-stack.ts
+++ infra/lib/user-stack.ts
@@ -6,14 +6,21 @@ export class UserStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const policy = new iam.ManagedPolicy(this, "AdminPolicy");
+    policy.addStatements(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: [
+        "*"
+      ],
+      resources: [
+        "*"
+      ]
+    }));
+
     const adminGroup = new iam.Group(this, "AdminGroup", {
       groupName: "playground-admins",
       managedPolicies: [
-        iam.ManagedPolicy.fromManagedPolicyName(
-          this,
-          "AdminAccess",
-          "AdministratorAccess",
-        ),
+        policy
       ],
     });
 
