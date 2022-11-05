f522791c18d82f6083259e2a6a1524ec4f96db6b
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2022-11-05 20:36:59 +0100

    feat: add admin user via stack (#4)

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { InfraStack } from '../lib/infra-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { InfraStack } from "../lib/infra-stack";
 
 const app = new cdk.App();
 
-new InfraStack(app, 'InfraStack', {
-  env: { account: '305747223196', region: 'eu-west-3' },
+new InfraStack(app, "InfraStack", {
+  env: { account: "305747223196", region: "eu-west-3" },
 });
