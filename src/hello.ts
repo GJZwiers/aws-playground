459556690b6159ef942f2c93f166050c25e74d37
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2022-11-06 12:30:00 +0100

    chore: test deno aws api

--- src/hello.ts
+++ src/hello.ts
@@ -1,17 +1,26 @@
 import {
-    APIGatewayProxyEventV2,
-    APIGatewayProxyResultV2,
-    Context,
-  } from "https://deno.land/x/lambda@1.9.0-1/mod.ts";
-  
-  // deno-lint-ignore require-await
-  export async function handler(
-    _event: APIGatewayProxyEventV2,
-    _context: Context,
-  ): Promise<APIGatewayProxyResultV2> {
-    return {
-      statusCode: 200,
-      headers: { "content-type": "text/html;charset=utf8" },
-      body: `Hello World! Sent from AWS CDK deno ${Deno.version.deno} 🦕`,
-    };
-  }
+  APIGatewayProxyEventV2,
+  APIGatewayProxyResultV2,
+  Context,
+} from "https://deno.land/x/lambda@1.9.0-1/mod.ts";
+import { ApiFactory } from "https://deno.land/x/aws_api@v0.7.0/client/mod.ts";
+import { Lambda } from "https://deno.land/x/aws_api@v0.7.0/services/lambda/mod.ts";
+
+export async function handler(
+  _event: APIGatewayProxyEventV2,
+  _context: Context,
+): Promise<APIGatewayProxyResultV2> {
+  const lambda = new ApiFactory({
+    region: "eu-west-3",
+  }).makeNew(Lambda);
+
+  const funs = await lambda.listFunctions({
+    MaxItems: 10,
+  });
+
+  return {
+    statusCode: 200,
+    headers: { "content-type": "text/html;charset=utf8" },
+    body: JSON.stringify(funs),
+  };
+}
