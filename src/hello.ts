8544cf5cb755d3453e42772bd24af9d4d6eb00cb
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2022-11-27 12:05:36 +0100

    try sdk from npm: url

--- src/hello.ts
+++ src/hello.ts
@@ -3,25 +3,23 @@ import {
   APIGatewayProxyResultV2,
   Context,
 } from "https://deno.land/x/lambda@1.27.0/mod.ts";
-import { ApiFactory } from "https://deno.land/x/aws_api@v0.7.0/client/mod.ts";
-import { Lambda } from "https://deno.land/x/aws_api@v0.7.0/services/lambda/mod.ts";
-
+import { LambdaClient, ListFunctionsCommand } from "npm:@aws-sdk/client-lambda";
+ 
 export async function handler(
   _event: APIGatewayProxyEventV2,
   _context: Context,
 ): Promise<APIGatewayProxyResultV2> {
-  const lambda = new ApiFactory({
-    region: "eu-west-3",
-  }).makeNew(Lambda);
 
-  const funs = await lambda.listFunctions({
+  const client = new LambdaClient({ region: "eu-west-3" });
+
+  const funs = await client.send(new ListFunctionsCommand({
     MaxItems: 10,
-  });
+  }));
   console.log(funs);
 
   return {
     statusCode: 200,
     headers: { "content-type": "text/html;charset=utf8" },
-    body: JSON.stringify(funs),
+    body: JSON.stringify(funs.Functions),
   };
 }
