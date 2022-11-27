2d43df3264e89f871c2a8350f0520cc9363253e2
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2022-11-27 12:53:06 +0100

    retry with aws_api module

--- src/hello.ts
+++ src/hello.ts
@@ -3,18 +3,27 @@ import {
   APIGatewayProxyResultV2,
   Context,
 } from "https://deno.land/x/lambda@1.28.1/mod.ts";
-import { LambdaClient, ListFunctionsCommand } from "npm:@aws-sdk/client-lambda";
+import { ApiFactory } from "https://deno.land/x/aws_api@v0.7.0/client/mod.ts";
+import { Lambda } from "https://deno.land/x/aws_api@v0.7.0/services/lambda/mod.ts";
+// import { LambdaClient, ListFunctionsCommand } from "npm:@aws-sdk/client-lambda";
  
 export async function handler(
   _event: APIGatewayProxyEventV2,
   _context: Context,
 ): Promise<APIGatewayProxyResultV2> {
+  const lambda = new ApiFactory({
+    region: "eu-west-3",
+  }).makeNew(Lambda);
 
-  const client = new LambdaClient({ region: "eu-west-3" });
-
-  const funs = await client.send(new ListFunctionsCommand({
+  const funs = await lambda.listFunctions({
     MaxItems: 10,
-  }));
+  });
+
+  // const client = new LambdaClient({ region: "eu-west-3" });
+
+  // const funs = await client.send(new ListFunctionsCommand({
+  //   MaxItems: 10,
+  // }));
   console.log(funs);
 
   return {
