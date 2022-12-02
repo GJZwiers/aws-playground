989b2c6ca62eed1e380103144b22d5aa889cb572
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2022-12-02 14:43:58 +0100

    upgrade module

--- src/listFunctions.ts
+++ src/listFunctions.ts
@@ -2,7 +2,7 @@ import {
   APIGatewayProxyEventV2,
   APIGatewayProxyResultV2,
   Context,
-} from "https://deno.land/x/lambda@1.28.1/mod.ts";
+} from "https://deno.land/x/lambda@1.28.3/mod.ts";
 // import { ApiFactory } from "https://deno.land/x/aws_api@v0.7.0/client/mod.ts";
 // import { Lambda } from "https://deno.land/x/aws_api@v0.7.0/services/lambda/mod.ts";
 import { LambdaClient, ListFunctionsCommand } from "npm:@aws-sdk/client-lambda";
