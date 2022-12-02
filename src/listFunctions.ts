64ea4adb79f2d7759a8e53f22e044f50a83e2a7b
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2022-12-02 14:52:02 +0100

    revert to deno native sdk

--- src/listFunctions.ts
+++ src/listFunctions.ts
@@ -3,28 +3,20 @@ import {
   APIGatewayProxyResultV2,
   Context,
 } from "https://deno.land/x/lambda@1.28.3/mod.ts";
-// import { ApiFactory } from "https://deno.land/x/aws_api@v0.7.0/client/mod.ts";
-// import { Lambda } from "https://deno.land/x/aws_api@v0.7.0/services/lambda/mod.ts";
-import { LambdaClient, ListFunctionsCommand } from "npm:@aws-sdk/client-lambda";
+import { ApiFactory } from "https://deno.land/x/aws_api@v0.7.0/client/mod.ts";
+import { Lambda } from "https://deno.land/x/aws_api@v0.7.0/services/lambda/mod.ts";
 
-// const lambda = new ApiFactory({
-//   region: "eu-west-3",
-// }).makeNew(Lambda);
-
-const client = new LambdaClient({ region: "eu-west-3" });
+const lambda = new ApiFactory({
+  region: "eu-west-3",
+}).makeNew(Lambda);
 
 export async function handler(
   _event: APIGatewayProxyEventV2,
   _context: Context,
 ): Promise<APIGatewayProxyResultV2> {
-  // const funs = await lambda.listFunctions({
-  //   MaxItems: 10,
-  // });
-  // console.log(funs);
-
-  const funs = await client.send(new ListFunctionsCommand({
+  const funs = await lambda.listFunctions({
     MaxItems: 10,
-  }));
+  });
   console.log(funs);
 
   return {
